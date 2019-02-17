dea0b89789996d867cb4cfd3dabe2be0debc22b9
RevoluPowered <gordon.a.macpherson@gmail.com>
2019-02-17 18:02:01 +0000

    WIP harmony include
    wil not reverse engineer libs at all as they are 1:1 compatible to stay conformant with the license.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
-
+extern "C" {
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include "system/int/sys_int.h"
+#include "peripheral/rtcc/plib_rtcc.h"
+#include "peripheral/devcon/plib_devcon.h"
+}
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
